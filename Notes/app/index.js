aab01658fe63090d04ec34663294b3c3e1e5d2c7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-01 15:09:22 -0400

    add: notes route

--- Notes/app/index.js
+++ Notes/app/index.js
@@ -1,19 +1,7 @@
-import {View, Text, StyleSheet, TextInput, Dimensions } from 'react-native'
-import { useState } from 'react'
+import {View, Text, StyleSheet } from 'react-native'
+import { Link } from 'expo-router'
 
 const styles = StyleSheet.create({
-    notes: {
-        padding: 20,
-        flex: 1,
-        outlineStyle: 'none',
-    },
-    title: {
-        fontSize: 40,
-        margin: 20,
-        borderBottomColor: '#999999',
-        borderBottomWidth: 5,
-        outlineStyle: 'none',
-    },
     view: {
         flex: 1
     }
@@ -22,13 +10,8 @@ const styles = StyleSheet.create({
 export default function Home() {
     return (
         <View style={styles.view}>
-            <TextInput placeholder='Title...' style={styles.title} placeholderTextColor="#AAAAAA" />
-            <TextInput
-            style={styles.notes}
-            multiline={true}
-            placeholder='notes...'
-            placeholderTextColor="#AAAAAA"
-            />
+            <Text>Homepage</Text>
+           <Link href={`/notes`}>View Notes</Link>
         </View>
     )
 }
