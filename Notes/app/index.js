5c6a0960eb353b20d6ec3a3b93059ded6ebacb9a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-01 00:02:10 -0400

    add: header and main content text areas with styling

--- Notes/app/index.js
+++ Notes/app/index.js
@@ -1,9 +1,34 @@
-import {View, Text } from 'react-native'
+import {View, Text, StyleSheet, TextInput, Dimensions } from 'react-native'
+import { useState } from 'react'
+
+const styles = StyleSheet.create({
+    notes: {
+        padding: 20,
+        flex: 1,
+        outlineStyle: 'none',
+    },
+    title: {
+        fontSize: 40,
+        margin: 20,
+        borderBottomColor: '#999999',
+        borderBottomWidth: 5,
+        outlineStyle: 'none',
+    },
+    view: {
+        flex: 1
+    }
+})
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.view}>
+            <TextInput placeholder='Title...' style={styles.title} placeholderTextColor="#AAAAAA" />
+            <TextInput
+            style={styles.notes}
+            multiline={true}
+            placeholder='notes...'
+            placeholderTextColor="#AAAAAA"
+            />
         </View>
     )
 }
