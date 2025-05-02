5398495afa39c43c5b72bdc9738c412eff4006b6
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-02 13:56:32 -0400

    add: save note function

--- Notes/app/hooks/useDatabase.js
+++ Notes/app/hooks/useDatabase.js
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useEffect, useState } from "react"
+import { v4 as uuid } from 'uuid'
 
 export default function useDatabase() {
     const [dbReady, setDbReady] = useState(false)
@@ -11,7 +12,7 @@ export default function useDatabase() {
             //setup db to save notes
             if (!initialized) {
                 await AsyncStorage.setItem('NOTES_DB_INIT', true)
-                await AsyncStorage.setItem('NOTES_IDS', JSON.stringify([]))
+                await AsyncStorage.setItem('NOTES_IDS', JSON.stringify(new Set()))
             }
 
             setDbReady(true)
@@ -19,4 +20,26 @@ export default function useDatabase() {
 
         initializeDB()
     }, [])
+
+    const saveNote = async (id, title, content) => {
+        if (!dbReady) return Promise.reject('Database not ready');
+        if (!id || !title || !content) return Promise.reject('Missing note data');
+        try {
+            //save note
+            const note = {id, title, content}
+            await AsyncStorage.setItem(`NOTE_${id}`, JSON.stringify(note))
+
+            //get ids and add note if it is not already listed
+            const idsData = await AsyncStorage.getItem('NOTES_IDS')
+            const ids = idsData ? new Set(JSON.parse(idsData)) : new Set()
+            if (!ids.has(id)) {
+                ids.add(id)
+                await AsyncStorage.setItem('NOTES_IDS', JSON.stringify(Array.from(ids)))
+            }
+            return note
+        } catch (error) {
+            console.log('error saving note', error)
+            return Promise.reject('error saving note');
+        }
+    }
 }
